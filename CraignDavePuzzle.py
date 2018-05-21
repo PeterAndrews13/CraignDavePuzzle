718014137a5b9c09c0d9f79a0ba69039afcbbc1c
Peter Andrews <34370665+PeterAndrews13@users.noreply.github.com>
2018-05-22 00:44:00 +0100

    Add files via upload

---
+++ CraignDavePuzzle.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2
+from PIL import Image
+from pytesser import *
+
+
+def saveVideo(videoLink):
+    yt = pytube.YouTube(videoLink)
+    yt.streams.first().download()
+    return yt.title
+
+
+def saveFrame(videoName,stillName):
+    cap = cv2.VideoCapture(videoName)
+    cap.set(1,180)# take the 180th frame
+    ret, frame = cap.read()
+    cv2.imwrite(stillName,frame)
+
+def readFrame(stillName):
+    im = Image.open(stillName)
+    text = image_file_to_string(stillName)
+    return text
+
+
